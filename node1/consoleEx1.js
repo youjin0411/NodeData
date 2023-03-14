51ed9ee364bac396aad9241391a791d5babded28
youjin <w2117@e-mirim.hs.kr>
2023-03-14 19:29:33 +0900

    nodeConsoleEx 예제"

---
+++ node1/consoleEx1.js
@@ -0,0 +1,7 @@
+// console: Node의 출력
+// 디버깅, 로그를 남길 때 이용
+// console.error(), console.warn()
+console.log("안녕하세요.")
+console.log("숫자입니다. %d", 19)
+console.log("객체입니다. %j", { name: "홍길동", age: 19 })
+console.log("문장입니다. %s", "안녕하세요.")
---
+++ node1/consoleEx2.js
@@ -0,0 +1,9 @@
+console.log('hello')
+console.log('hello', 'bye')
+console.warn(`this line ${'can make error'}`);
+var person = {
+    name: '소녀시대',
+    age: 20
+};
+console.log("자바스크립트 객체입니다 %j", person)
+console.dir(person)
