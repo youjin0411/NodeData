9e847ef6a5a5b3c0aa1d3ff617f9a5deb45e09f7
youjin <w2117@e-mirim.hs.kr>
2023-03-14 20:07:17 +0900

    array 및 let var의 차이

---
+++ node1/arrayEx1.js
@@ -0,0 +1,11 @@
+//배열 선언 : new Array, []
+const number = [1, 2, 3]
+const strings = ['hello', 'bye', 'welcome']
+
+const number2 = new Array(1, 2, 3);
+const strings2 = new Array('hello', 'bye', 'welcome');
+
+console.log('number: ', number);
+console.log('number2: ', number2);
+console.log('strings: ', strings);
+console.log('strings2: ', strings2);
---
+++ node1/letEx1.js
@@ -0,0 +1,17 @@
+//let, var의 차이점 알기
+let fruit = 'apple';
+console.log('fruit: ', fruit);
+
+fruit = `grape`;
+console.log('fruit: ', fruit);
+
+//let fruit = 'hello';
+
+var variable1 = 'hello';
+var variable2 = 'bye';
+
+console.log(variable1);
+
+//let과 var의 차이점은 
+//var은 위에서 사용한 변수명을 다시 선언해서 수정할 수 있지만
+//let은 위에서 사용한 변수명을 다시 선언해서 수정할 수 없다.
